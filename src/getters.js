d48a1180b5bf820f00ca1a802f873c93d64bc066
hao.cheong <61342120+HaoCheong@users.noreply.github.com>
2023-10-03 11:15:17 +1100

    Refactored inky

--- src/getters.js
+++ src/getters.js
@@ -1,62 +1,65 @@
-import fs, { existsSync } from "fs";
-import yaml from "js-yaml";
-
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const getOptions = () => {
-  /**
-   * Get the options as a JS object from the options file
-   */
-  const paths = getAbsolutePaths();
-  const options = yaml.load(fs.readFileSync(paths.optionsPath, "utf8"));
-  return options;
-};
+import fs from "fs";
+import yaml from "js-yaml";
 
-export const getTags = () => {
-  /**
-   * Get all the tags raw the template file
-   */
-
-  const paths = getAbsolutePaths();
-  const r_template = fs.readFileSync(paths.templatePath, "utf8");
-  const template_arr = r_template.split(" }}");
-  const all_tags = template_arr.map((x) => {
-    return x.split("{{ ")[1];
-  });
-  const tags_raw = all_tags.slice(0, -1);
-  return tags_raw;
+export const getAbsolutePaths = () => {
+    /**
+     * Get all the required files absolute paths
+     */
+  
+    return {
+        workspacePath: resolve(__dirname, "../workspace"),
+        outputPath: resolve(__dirname, "../workspace/output"),
+        optionsPath: resolve(__dirname, "../workspace/options.yml"),
+        templatePath: resolve(__dirname, "../workspace/template.txt"),
+    };
 };
 
 export const getTemplate = () => {
-  /**
-   * Get the template file as a raw string
-   */
-  const paths = getAbsolutePaths();
-  const r_template = fs.readFileSync(paths.templatePath, "utf8");
-  return r_template;
+    /**
+     * Get the template file as a raw string
+     */
+
+    const paths = getAbsolutePaths();
+    const r_template = fs.readFileSync(paths.templatePath, "utf8");
+    return r_template;
 };
 
 export const getTime = () => {
-  /**
-   * Get current time as a localeString
-   */
-
-  const currDatetime = new Date();
-  return currDatetime.toLocaleString();
+    /**
+     * Get current time as a localeString
+     */
+  
+    const currDatetime = new Date();
+    return currDatetime.toLocaleString();
 };
 
-export const getAbsolutePaths = () => {
-  /**
-   * Get all the required files absolute paths
-   */
+export const getOptions = () => {
+    /**
+     * Get the options as a JS object from the options file
+     */
 
-  return {
-    workspacePath: resolve(__dirname, "../workspace"),
-    outputPath: resolve(__dirname, "../workspace/output"),
-    optionsPath: resolve(__dirname, "../workspace/options.yml"),
-    templatePath: resolve(__dirname, "../workspace/template.txt"),
+    const paths = getAbsolutePaths();
+    const options = yaml.load(fs.readFileSync(paths.optionsPath, "utf8"));
+    return options;
+};
+
+export const getTags = () => {
+    /**
+     * Get all the tags raw the template file
+     */
+  
+    const paths = getAbsolutePaths();
+    const r_template = fs.readFileSync(paths.templatePath, "utf8");
+    const template_arr = r_template.split(" }}");
+    const all_tags = template_arr.map((x) => {
+      return x.split("{{ ")[1];
+    });
+    const tags_raw = all_tags.slice(0, -1);
+    return tags_raw;
   };
-};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-#!/usr/bin/env node
 
-// Animation Imports
-import chalk from "chalk";
-import inquirer from "inquirer";
-import gradient from "gradient-string";
-import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 
 import { fileURLToPath } from "url";
@@ -15,211 +9,162 @@ const __dirname = dirname(__filename);
 
 import fs from "fs";
 
-import {
-  getOptions,
-  getTags,
-  getAbsolutePaths,
-  getTemplate,
-  getTime,
-} from "./getters.js";
 
 import {
-  inputTitle,
-  inputConcat,
-  inputManual,
-  inputContinue,
-  inputAnswers,
-  inputMulti,
-} from "./inputs.js";
+    inputConcat,
+    inputTitle,
+    inputContinue,
+    repeatedChoice,
+    selectRandomChoice
+} from "./inputs.js"
 
 import {
-  generateRecentWriting,
-  selectRandomChoice,
-  diffTags,
-  parseTags,
-  display,
-  handleResponse,
-} from "./helpers.js";
+    getAbsolutePaths,
+    getTime,
+    getOptions,
+    getTags,
+} from "./getters.js"
+
+import {
+  validatePrestart,
+  validateWorkspace
+} from "./validators.js"
 
 import {
   checkReqFiles,
-  validateWorkspace,
-  validatePrestart,
-} from "./validators.js";
-
-import { exit } from "process";
-
-const saveWriting = async (title, concat, responses) => {
-  /**
-   * Save writing to a file (Title: title.txt)
-   */
-
-  const new_page_path = resolve(__dirname, `../workspace/output/${title}.txt`);
-  const content = await generateRecentWriting(responses);
-
-  if (!fs.existsSync(new_page_path)) {
-    fs.writeFileSync(new_page_path, "");
-  }
-
-  if (concat) {
-    console.log(chalk.bgGreen(` +++ Appending to "${new_page_path}" +++ \n`));
-    fs.appendFileSync(new_page_path, `\n${content}`);
-  } else {
-    console.log(chalk.bgGreen(` +++ Saving to "${new_page_path}" +++ \n`));
-    fs.writeFileSync(new_page_path, content);
-  }
-};
-
-const runThrough = async (options, tags, meta) => {
-  /**
-   * Runs through all the tags and append to responses
-   */
-
-  console.log(options);
-  console.log(tags);
-
-  let responses = [];
-
-  while (tags.length > 0) {
-    let tag_counter = 0;
-    const curr_tag_raw = tags[tag_counter];
-    const curr_tag_obj = parseTags(curr_tag_raw);
-
-    // Show current writing
-    const curr_writing = generateRecentWriting(responses);
-    display(curr_writing, meta);
-
-    let answer = "";
-
-    // Special case tags check
-    switch (curr_tag_obj.option) {
-      case "_time":
-        answer = getTime();
-        break;
-      case "_manual":
-        answer = await inputManual();
-        break;
-    }
-    console.log("answer", answer)
-
-    // Check tag types
-    if (answer === "") {
-      switch (curr_tag_obj.type) {
-        case "-r":
-          answer = selectRandomChoice(options[curr_tag_obj.option]);
-          break;
-        case "-l":
-          answer = await inputMulti(options, curr_tag_obj, meta, responses);
-          break;
-        default:
-          console.log("HERE 3", curr_tag_obj, options, options[curr_tag_obj.option])
-          answer = await inputAnswers(options[curr_tag_obj.option]);
-          break;
-      }
-    }
-
-    console.log("answer 2", answer)
-    if (answer === "") {
-      continue;
-    }
-
-    const response = await handleResponse(answer);
-    responses.push(response);
-    tags.shift();
-    tag_counter += 1;
-  }
-
-  await saveWriting(meta.title, meta.concat, responses);
-};
+  display,
+  generateRecentWriting,
+  parseTags,
+  saveWriting
+} from "./helpers.js"
 
 const init = () => {
-  /**
-   * Initialisation of workspace
-   */
+    /**
+     * Initialisation of workspace
+     */
+  
+    if (process.argv[2] === "init") {
+        if (checkReqFiles()) {
+            console.log(`
+            Required files already exist
+            To run inky, run the command:
+                inky
+                `);
+            process.exit(1);
+        }
+        const paths = getAbsolutePaths();
+        const defaultOptions = resolve(__dirname, `./_default/options.yml`);
+        const defaultTemplate = resolve(__dirname, `./_default/template.txt`);
+        fs.mkdirSync(paths.workspacePath);
+        fs.mkdirSync(paths.outputPath);
+        fs.writeFileSync(paths.optionsPath, "");
+        fs.writeFileSync(paths.templatePath, "");
+        fs.copyFileSync(defaultOptions, paths.optionsPath);
+        fs.copyFileSync(defaultTemplate, paths.templatePath);
 
-  if (process.argv[2] === "init") {
-    if (checkReqFiles()) {
-      console.log(`
-      Required files already exist
-      To run inky, run the command:
-          inky
+        const msg = `Inky <:E`;
+        console.log(figlet.textSync(msg));
+
+        console.log(`
+        Initialise complete: options.yml and template.txt added to workspace
+        To begin, run the command:
+            inky
             `);
-      exit(1);
+        process.exit(0);
     }
-    const paths = getAbsolutePaths();
-    const defaultOptions = resolve(__dirname, `./_default/options.yml`);
-    const defaultTemplate = resolve(__dirname, `./_default/template.txt`);
-    fs.mkdirSync(paths.workspacePath);
-    fs.mkdirSync(paths.outputPath);
-    fs.writeFileSync(paths.optionsPath, "");
-    fs.writeFileSync(paths.templatePath, "");
-    fs.copyFileSync(defaultOptions, paths.optionsPath);
-    fs.copyFileSync(defaultTemplate, paths.templatePath);
-
-    const msg = `Inky <:E`;
-    console.log(figlet.textSync(msg));
-
-    console.log(`
-    Initialise complete: options.yml and template.txt added to workspace
-    To begin, run the command:
-        inky
-        `);
-    exit(0);
-  }
 };
 
-// ========== Pre-start ==========
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+  
+
 
 const main = async () => {
-  // ========== PRESTART ===========
 
-  // Create files when added an init
-  await init();
+    // Initialisation check
+    await init()
 
-  // Check all required files and directories exist
-  validatePrestart();
+    // Validation of prestart files
+    validatePrestart() 
+  
+    // Get and check input of potential work space
+    let meta = { title: "", continue: true, concat: false };
+    meta.title = await inputTitle();
+    let options = getOptions();
+    let tags = getTags();
 
-  // ========== TAG & TEMPLATE PROCESSING ==========
+    validateWorkspace(options, tags, meta);
 
-  // Get options available and get tags
-  let options = getOptions();
-  let tags = getTags();
+    meta.concat = await inputConcat();
+    while (meta.continue) {
 
-  // Initial meta values setup
-  const meta = { title: "", continue: true, concat: false };
-  meta.title = await inputTitle();
-  meta.concat = await inputConcat();
+        let responses = [];
+        while (tags.length > 0) {
+            let tag_counter = 0;
+            
 
-  validateWorkspace(options, tags, meta);
+            let curr_writing = generateRecentWriting(responses);
+            display(curr_writing, meta);
 
-  // ========== RUN THROUGH ==========
-  while (meta.continue) {
-    //Run through all tags
-    await runThrough(options, tags, meta);
+            const curr_tag_raw = tags[tag_counter];
+            const curr_tag_obj = parseTags(curr_tag_raw);
 
-    //Reset all values
-    options = getOptions();
-    tags = getTags(); 
+            // Check special tags
+            switch (curr_tag_obj.option) {
+                case "_time":
+                    let answer = getTime();
+                    responses.push(answer);
+                    tags.shift();
+                    tag_counter += 1;
+                    continue
+            }
 
-    //Get continue
-    meta.continue = await inputContinue();
+            // Check tag options
+            let resp = ""
+            switch (curr_tag_obj.type) {
+                case "-r":
+                    resp = selectRandomChoice(options[curr_tag_obj.option]);
+                    break;
+                case "-l":
+                    //Repeated input
+                    resp = await repeatedChoice(options, curr_tag_obj, curr_tag_obj.args[0], responses, meta)
+                    break
+                default:
+                    //Single Choice
+                    resp = await repeatedChoice(options, curr_tag_obj, 1, responses, meta)
+                    break
+            }
+            responses.push(resp)
+            tags.shift();
+            tag_counter += 1;
 
-    //Overwrite title for new files
-    if (meta.continue && !meta.concat) {
-      meta.title = await inputTitle();
+            curr_writing = generateRecentWriting(responses);
+            display(curr_writing, meta);
+        }
+
+        await saveWriting(meta.title, meta.concat, responses);
+        options = getOptions();
+        tags = getTags();
+
+        meta.continue = await inputContinue()
+        if (!meta.concat && meta.continue) {
+          meta.title = await inputTitle();
+        }
     }
-  }
-};
+}
 
-await main();
-
-// ========== Post ==========
-
-/** EXTRA
- * TODO - Create a validate function to validate all files (Add no tagless templates)
- * TODO - Move all input prompts to inputs.js
- * TODO - Rename all inputs into input<prompt>() instead
- * TODO - Move all basic getters into getters
- * TODO - Add JS Docs to all of them
- * TODO - Convert all snake case to camel case
- */
+main()
