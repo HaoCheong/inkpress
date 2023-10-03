8bccf5339f4f8fea303645672dd8fa085acc9b9d
hao.cheong <hao.cheong@kgit.com.au>
2023-10-03 11:15:17 +1100

    Refactored inky

--- src/helpers.js
+++ src/helpers.js
@@ -1,90 +1,127 @@
-import { getTags, getTemplate } from "./getters.js";
-import { inputManual } from "./inputs.js";
 import chalk from "chalk";
+
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import fs from "fs";
+
 const specialTags = ["_time", "_manual"];
 
+import {
+    getAbsolutePaths,
+    getTemplate,
+    getTags,
+} from "./getters.js"
+export const display = (writing, meta) => {
+    /**
+     * Display content in a terminal box
+     */
+  
+    console.clear();
+    console.log(`${chalk.bold("Title")}: ${meta.title}`);
+    console.log(
+      chalk.bgBlue("<<<<<<<<<<<<<<<<<<<< CURRENT >>>>>>>>>>>>>>>>>>>>\n")
+    );
+    console.log(`${writing}\n`);
+    console.log(
+      chalk.bgBlue("<<<<<<<<<<<<<<<<<<<<<< END >>>>>>>>>>>>>>>>>>>>>>\n")
+    );
+};
+
 export const generateRecentWriting = (responses) => {
-  /**
-   * Given the responses, generate the writing via tag replacement
-   */
+    /**
+     * Given the responses, generate the writing via tag replacement
+     */
 
-  let template = getTemplate();
-  const all_tags = getTags();
-
-  for (let i = 0; i < responses.length; ++i) {
-    template = template.replace(`{{ ${all_tags[i]} }}`, responses[i]);
-  }
-  return template;
+    let template = getTemplate();
+    const all_tags = getTags();
+  
+    for (let i = 0; i < responses.length; ++i) {
+      template = template.replace(`{{ ${all_tags[i]} }}`, responses[i]);
+    }
+    return template;
 };
 
-export const selectRandomChoice = (choices) => {
-  /**
-   * Get a random answer from the given option list
-   */
-
-  const index = Math.floor(Math.random() * choices.length);
-  return choices[index];
-};
-
-export const handleResponse = async (answer, choices) => {
-  if (answer === "RANDOM") {
-    return selectRandomChoice(choices);
-  } else if (answer === "MANUAL INPUT") {
-    return await inputManual();
-  } else {
-    return answer;
-  }
+export const checkReqFiles = () => {
+    /**
+     * Check if all the required files exist
+     */
+    const paths = getAbsolutePaths();
+    return (
+        fs.existsSync(paths.workspacePath) &&
+        fs.existsSync(paths.outputPath) &&
+        fs.existsSync(paths.optionsPath) &&
+        fs.existsSync(paths.templatePath)
+    );
 };
 
 export const diffTags = (options, tags) => {
-  /**
-   * Return the difference in tags between options on template files
-   */
-
-  const optionKeys = Object.keys(options).sort();
-
-  //Get a set of tags removing dupes
-  const allTags = [
-    ...new Set(
-      tags.map((tag) => {
-        return tag.split(" ")[0];
-      })
-    ),
-  ].sort();
-
-  //Filter out any special tags
-  const tagOptions = allTags.filter((tag) => !specialTags.includes(tag));
-
-  //Get the difference between tagOption and optionKeys
-  const diff = tagOptions.filter((tag) => !optionKeys.includes(tag));
-  return diff;
+    /**
+     * Return the difference in tags between options on template files
+     */
+  
+    const optionKeys = Object.keys(options).sort();
+  
+    //Get a set of tags removing dupes
+    const allTags = [
+      ...new Set(
+        tags.map((tag) => {
+          return tag.split(" ")[0];
+        })
+      ),
+    ].sort();
+  
+    //Filter out any special tags
+    const tagOptions = allTags.filter((tag) => !specialTags.includes(tag));
+  
+    //Get the difference between tagOption and optionKeys
+    const diff = tagOptions.filter((tag) => !optionKeys.includes(tag));
+    return diff;
 };
+
 
 export const parseTags = (tag) => {
-  /**
-   * Converts raw tags into a tag object
-   */
+    /**
+     * Converts raw tags into a tag object
+     */
+  
+    const tag_details = tag.split(" ");
+    return {
+      option: tag_details[0],
+      type: tag_details[1],
+      args: tag_details.slice(2),
+    };
+};
 
-  const tag_details = tag.split(" ");
-  return {
-    option: tag_details[0],
-    type: tag_details[1],
-    args: tag_details.slice(2),
+export const checkResponse = (resp) => {
+    if (resp === "") {
+        console.error("Response cannot be empty")
+        return false
+    }
+    return true
+}
+
+
+export const saveWriting = async (title, concat, responses) => {
+    /**
+     * Save writing to a file (Title: title.txt)
+     */
+  
+    const new_page_path = resolve(__dirname, `../workspace/output/${title}.txt`);
+    const content = await generateRecentWriting(responses);
+  
+    if (!fs.existsSync(new_page_path)) {
+      fs.writeFileSync(new_page_path, "");
+    }
+  
+    if (concat) {
+      console.log(chalk.bgGreen(` +++ Appending to "${new_page_path}" +++ \n`));
+      fs.appendFileSync(new_page_path, `\n${content}`);
+    } else {
+      console.log(chalk.bgGreen(` +++ Saving to "${new_page_path}" +++ \n`));
+      fs.writeFileSync(new_page_path, content);
+    }
   };
-};
-
-export const display = (writing, meta) => {
-  /**
-   * Display content in a terminal box
-   */
-
-  console.clear();
-  console.log(`${chalk.bold("Title")}: ${meta.title}`);
-  console.log(
-    chalk.bgBlue("<<<<<<<<<<<<<<<<<<<< CURRENT >>>>>>>>>>>>>>>>>>>>\n")
-  );
-  console.log(`${writing}\n`);
-  console.log(
-    chalk.bgBlue("<<<<<<<<<<<<<<<<<<<<<< END >>>>>>>>>>>>>>>>>>>>>>\n")
-  );
-};
--- src/inputs.js
+++ src/inputs.js
@@ -1,123 +1,144 @@
-import chalk from "chalk";
 import inquirer from "inquirer";
-import { generateRecentWriting, display, handleResponse } from "./helpers.js";
 
-export const inputTitle = async () => {
-  /**
-   * Prompt getting the title of the current run through
-   */
-
-  const details = await inquirer.prompt({
-    name: "title",
-    type: "input",
-    message: "Title: ",
-  });
-  return details.title;
-};
+import {
+  display,
+  generateRecentWriting,
+} from "./helpers.js"
 
 export const inputConcat = async () => {
-  /**
-   * Prompt for concatenating values into one file
-   */
-
-  const res = await inquirer.prompt({
-    name: "concat",
-    type: "list",
-    message: "Concatenate in one file? ",
-    choices: ["Yes", "No"],
-  });
-  return res.concat === "Yes";
-};
-
-export const inputManual = async () => {
-  /**
-   * Get the user's manual user input
-   */
-
-  const details = await inquirer.prompt({
-    name: "manualPrompt",
-    type: "input",
-    message: "Manual Input:",
-  });
-  return details.manualPrompt;
-};
-
-export const inputContinue = async () => {
-  /**
-   * Get the user's continue prompt
-   */
-
-  const res = await inquirer.prompt({
-    name: "continue",
-    type: "list",
-    message: "Continue writing? ",
-    choices: ["Yes", "No"],
-  });
-  return res.continue === "Yes";
+    /**
+     * Prompt for concatenating values into one file
+     */
+  
+    const res = await inquirer.prompt({
+        name: "concat",
+        type: "list",
+        message: "Concatenate in one file? ",
+        choices: ["Yes", "No"],
+    });
+    return res.concat === "Yes";
 };
 
 export const inputAnswers = async (options) => {
-  /**
-   * Selector of choices
-   */
+    /**
+     * Selector of choices
+     */
+  
+    if (options === undefined) {
+      return ""
+    }
+  
+    const choices = options.concat(["", "RANDOM", "MANUAL INPUT", ""]);
+    const question = await inquirer.prompt({
+        name: "answer",
+        type: "list",
+        message: "INPUT: ",
+        choices: choices,
+    });
 
-  if (options === undefined) {
-    return ""
-  }
+    if (question.answer === "RANDOM") {
+        return selectRandomChoice(options);
+    } else if (question.answer === "MANUAL INPUT") {
+        return inputManual()
+    }
 
-  const choices = options.concat(["", "RANDOM", "MANUAL INPUT"]);
-  const question = await inquirer.prompt({
-    name: "answer",
-    type: "list",
-    message: "INPUT: ",
-    choices: choices,
-  });
-  return question.answer;
+    return question.answer;
+    
 };
 
-export const inputMulti = async (options, curr_tag_obj, meta, responses) => {
-  /**
-   * Iterative input for multiple of the same tag
-   */
-
-  const delim = curr_tag_obj.args[1];
-  let next_answer = "";
-  let new_answer = "";
-
-  for (let i = curr_tag_obj.args[0]; i !== 0; i--) {
-    switch (curr_tag_obj.option) {
-      case "_manual":
-        next_answer = await inputManual();
-        break;
-      default:
-        next_answer = await inputAnswers(
-          options[curr_tag_obj.option].concat(["", "DONE"])
-        );
-        break;
-    }
-
-    if (next_answer === "DONE") {
-      return new_answer;
-    }
-
-    new_answer = await handleResponse(new_answer, options[curr_tag_obj.option]);
-
-    if (delim === "\\n") {
-      new_answer = `${new_answer} \n ${next_answer}`;
-    } else {
-      new_answer = `${new_answer} ${delim} ${next_answer}`;
-    }
-
-    const new_response = [...responses, new_answer];
-    const curr_writing = await generateRecentWriting(new_response);
-    display(curr_writing, meta);
-
-    if (i < 0) {
-      console.log(chalk.yellow("Select DONE or continue adding\n"));
-    } else {
-      console.log(`Remaining: ${chalk.yellow(i - 1)}\n`);
-    }
-  }
-
-  return new_answer;
+export const inputTitle = async () => {
+    /**
+     * Prompt getting the title of the current run through
+     */
+  
+    const details = await inquirer.prompt({
+        name: "title",
+        type: "input",
+        message: "Title: ",
+    });
+    return details.title;
 };
+
+export const inputContinue = async () => {
+    /**
+     * Get the user's continue prompt
+     */
+  
+    const res = await inquirer.prompt({
+        name: "continue",
+        type: "list",
+        message: "Continue writing? ",
+        choices: ["Yes", "No"],
+    });
+    return res.continue === "Yes";
+};
+
+export const inputManual = async () => {
+    /**
+     * Get the user's manual user input
+     */
+  
+    const details = await inquirer.prompt({
+        name: "manualPrompt",
+        type: "input",
+        message: "Manual Input:",
+    });
+    return details.manualPrompt;
+};
+
+export const repeatedChoice = async (options, curr_tag_obj, repeatCount, responses, meta) => {
+    
+    let resp_arr = []
+    let counter = repeatCount
+    let resp = ""
+    let delim = ""
+
+    if (curr_tag_obj.type !== undefined) {
+        delim = curr_tag_obj.args[1]
+        if (delim === "\\n") {
+            delim = "\n"
+        } else {
+            delim = ` ${delim} `
+        }
+    }
+
+    let curr_resp = ""
+    while (counter !== 0) {
+
+        if (curr_tag_obj.option === "_manual") {
+            curr_resp = await inputManual();
+        } else {
+            curr_resp = await inputAnswers(options[curr_tag_obj.option].concat(["", "DONE"])) 
+        }
+
+        if (curr_resp === "DONE") {
+            break
+        }
+
+        if (curr_resp !== "") {
+            resp_arr.push(curr_resp)
+            counter -= 1
+        }
+
+        let temp_resp = resp_arr.join(delim)
+        let temp_responses = [...responses]
+        temp_responses.push(temp_resp)
+        let temp_writing = generateRecentWriting(temp_responses)
+        console.log("TEMPO", temp_writing)
+        display(temp_writing, meta)
+
+    }
+    resp = resp_arr.join(delim)
+    return resp
+
+}
+
+export const selectRandomChoice = (choices) => {
+    /**
+     * Get a random answer from the given option list
+     */
+    
+    const index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+};
+    
